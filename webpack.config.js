f0c685bfba53f20935ef79e79985792786ff79aa
jairisonsouza <jairo.tinformatica@gmail.com>
2023-07-23 19:41:11 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app/dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader','css-loader']}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/src/app.html',
+            filename: 'app.html',
+            hash: true
+        })
+    ]
+};
